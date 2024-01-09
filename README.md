2e61de22188fde932281848f51961c5ab9d6e938
IsoCodeCrafter <ismaildogan07@gmail.com>
2024-01-09 20:44:58 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<h5>First Comment</h5>
+
+<h5>when you want to use Test install=> npm i axios@^0.27 </h5>
+# UnitTest
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Scoops from "./components/Scoops";
+import Toppings from "./components/Toppings";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div>
+      {/* Çeşitler */}
+      <Scoops />
+      {/* Toppings */}
+      <Toppings />
+      {/* Form */}
+      <Form />
+    </div>
+  );
+}
+
+export default App;
